1191731076820dc982eca6289580421f543623b6
qwer643 <inst.rabot@gmail.com>
2022-09-25 20:42:18 +0300

    clear code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
